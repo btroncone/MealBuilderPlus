9910b53207ab6e33193560ba6ba06f43d792d0d6
btroncone <btroncone@gmail.com>
2014-08-07 00:22:41 -0400

    Updated unit test for 'controller as'

--- MealBuilderPlus.Tests/Specs/mealInformationControllerSpec.js
+++ MealBuilderPlus.Tests/Specs/mealInformationControllerSpec.js
@@ -1,20 +1,19 @@
-beforeEach(module("mealBuilderPlusApp"));
-var $rootScope, $controller, $httpBackend, scope;
-beforeEach(inject(function(_$rootScope_, _$controller_, _$httpBackend_){
-
-    $rootScope = _$rootScope_;
-    $controller = _$controller_;
-    $httpBackend = _$httpBackend_;
-    scope = $rootScope.$new();
-}));
-afterEach(function(){
-
-    $httpBackend.verifyNoOutstandingExpectation();
-    $httpBackend.verifyNoOutstandingRequest();
-});
-
 describe("the mealInformationController", function(){
+    beforeEach(module('mealBuilderPlusApp'));
+    var $rootScope, $controller, $httpBackend, scope;
+    beforeEach(inject(function(_$rootScope_, _$controller_, _$httpBackend_){
 
+        $rootScope = _$rootScope_;
+        $controller = _$controller_;
+        $httpBackend = _$httpBackend_;
+        scope = $rootScope.$new();
+    }));
+
+    afterEach(function(){
+
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
     beforeEach(function(){
         $httpBackend.when("GET", "/api/meals").respond([{},{},{}]);
         $controller("mealInformationController as vm", {
@@ -24,7 +23,7 @@ describe("the mealInformationController", function(){
     });
 
     it("should retrieve meals to a list", function(){
-        expect(scope.mealList.length).toBe(3);
+        expect(scope.vm.mealList.length).toBe(3);
     });
 
 });
--- MealBuilderPlus.Web/app/app.js
+++ MealBuilderPlus.Web/app/app.js
@@ -6,6 +6,4 @@
             "ngAnimate",
             "angularUtils.directives.dirPagination"
         ]);
-
-
 }());
